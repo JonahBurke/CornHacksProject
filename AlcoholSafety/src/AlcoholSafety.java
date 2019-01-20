101b35e486101e3d7ad6077f74509c7271bbd62b
JonahBurke <jonahburke777@gmail.com>
2019-01-19 22:15:21 -0600

    Basic gui with no backend
    Also still has a bunch of test cases still in there

--- AlcoholSafety/src/AlcoholSafety.java
+++ AlcoholSafety/src/AlcoholSafety.java
@@ -1,40 +1,114 @@
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GraphicsConfiguration;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.util.Scanner;
-
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 /**
  * Authors: Jonah Burke and Ethan Weber Date: 1/19/2019
  * 
  * This is our project for the CornHacks event at UNL.
  */
 
-public class AlcoholSafety extends JFrame{
-	static GraphicsConfiguration gc;
-	static JTextField weight, height;
+public class AlcoholSafety extends Application{
+	
+	@Override
+    public void init() {
+		// Put something in here if you need setup.
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+    	// Initializing default/custom objects and variables
+    	int maxWidth = 250;
+    	int stdHeight = 25;
+    	
+        // Initializing javafx objects
+    	Scene scene;
+        StackPane root = new StackPane();
+        root.setAlignment(Pos.TOP_LEFT);
+        TextField gender = new TextField("Male or Female?");
+        TextField weight = new TextField("Weight (lbs)");
+        TextField searchbar = new TextField("Search for Alcohol");
+        Font labelFont = new Font(15);
+        Label info = new Label("Body Information");
+        info.setFont(labelFont);
+        Label search = new Label("Alcohol Search");
+        search.setFont(labelFont);
+        Button execSearch = new Button("Search");
+        ListView<String> alcoholList = new ListView<>();
+        ObservableList<String> items = FXCollections.observableArrayList ("Single", "Double", "Suite", "Family App");
+        alcoholList.setItems(items);
+        
+        info.setTranslateY(0);
+        info.setTranslateX(0);
+        gender.setTranslateY(info.getTranslateY() + stdHeight);
+        gender.setMaxWidth(maxWidth);
+        gender.setTranslateX(info.getTranslateX());
+        weight.setTranslateY(gender.getTranslateY() + stdHeight);
+        weight.setMaxWidth(maxWidth);
+        weight.setTranslateX(info.getTranslateX());
+        search.setTranslateY(weight.getTranslateY() + stdHeight);
+        search.setTranslateX(info.getTranslateX());
+        searchbar.setTranslateY(search.getTranslateY() + stdHeight);
+        searchbar.setTranslateX(info.getTranslateX());
+        searchbar.setMaxWidth(maxWidth);
+        execSearch.setTranslateY(searchbar.getTranslateY() + stdHeight);
+        execSearch.setTranslateX(info.getTranslateX());
+        alcoholList.setTranslateY(execSearch.getTranslateY() + stdHeight);
+        alcoholList.setTranslateX(info.getTranslateX());
+        alcoholList.setMaxWidth(maxWidth);
+        alcoholList.setMaxHeight(150);
+        
+        // Handle click on "Search" button
+        execSearch.setOnAction((ActionEvent) -> {
+        	System.out.println("Lets boogie");
+            root.getChildren().add(alcoholList);
+        });
+        // Handle the list of possible drinks
+        alcoholList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                
+            }
+        });
+        
+       
+        // Add all the nodes to the StackPane
+        root.getChildren().add(info);
+        root.getChildren().add(gender);
+        root.getChildren().add(weight);
+        root.getChildren().add(search);
+        root.getChildren().add(searchbar);
+        root.getChildren().add(execSearch);
+        
+        // Setup the scene
+        scene = new Scene(root, 400, 400);
+        // Adding the title to the window (primaryStage)
+        primaryStage.setTitle("Drink Safely");
+        primaryStage.setScene(scene);
+        // Show the window(primaryStage)
+        primaryStage.show();
+    }
+    
+    @Override
+    public void stop() {
+        Platform.exit();
+    }
+	
 	public static void main(String[] args) {
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int screenHeight = (int) screenSize.getHeight();
-		int screenWidth = (int) screenSize.getWidth();
-		JFrame frame = new JFrame(gc);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		launch(args);
 		
-		frame.setTitle("Alcohol Safety");
-		frame.setSize(screenWidth/2, screenHeight/2);
-		frame.setLocation(screenWidth/4, screenHeight/4);
-		
-		
-		frame.getContentPane().setLayout(new FlowLayout());
-		
-		frame.pack();
-		frame.setVisible(true);
-		
-		
+		/*
 		Scanner scan = new Scanner(System.in);
 		String input = null;
 		boolean decided = true;
@@ -59,9 +133,9 @@ public class AlcoholSafety extends JFrame{
 		} else { // Update Drinks Consumed
 			System.out.println("Enter Drink Name");
 			input = scan.nextLine();
-//			if (/* Drink is not found in database */ ) {
-//				// TODO: Prompt user for drink type (whisky, rum, etc.) and/or other necessary info
-//			}
+			if ( Drink is not found in database  ) {
+				// TODO: Prompt user for drink type (whisky, rum, etc.) and/or other necessary info
+			}
 
 			System.out.println("Amount:");
 			//Probably have to store this in a double or int variable or somethign
@@ -69,6 +143,7 @@ public class AlcoholSafety extends JFrame{
 
 			// TODO: Calculate the amount of alcohol/standard drinks consumed so far and how much more cna be handled
 		}
+		*/
 
 	}
 
